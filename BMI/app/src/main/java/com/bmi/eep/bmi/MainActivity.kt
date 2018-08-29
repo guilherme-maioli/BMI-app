3ac1d1f1cfbdb48d1621c06148f196aae129f02d
Guilherme Maioli <guitue@gmail.com>
2018-08-28 22:07:33 -0300

    create project

---
+++ BMI/app/src/main/java/com/bmi/eep/bmi/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.bmi.eep.bmi
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
